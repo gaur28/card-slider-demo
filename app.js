3c504a7c8eed83afdb675a9fb1b41b885a70cdc4
Tushar Gaur <gaurtushar5567@gmail.com>
2023-01-27 18:19:52 +0530

    added authentication functionality

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const {render} = require('ejs')
 const express = require('express');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session');
 const fs = require('fs');
 const mongodb = require('mongodb');
 const path = require('path');
 const routes = require('./route/route');
 const db = require('./data/database');
 
+const MongodbStore = MongoDBStore(session);
+const sessionStore = new MongodbStore({
+    uri: 'mongodb://localhost:27017',
+    databaseName: 'books',
+    collection: 'session'
+});
 
 const app = express();
 app.use(express.static('public'));
@@ -13,6 +21,17 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'view'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'super-secret',
+    resave:false,
+    saveUninitialized: false,
+    store:sessionStore,
+    cookie: {
+        maxAge:30*24*60*60*1000 
+
+    }
+}))
+
 app.use(express.json());
 app.use(routes);
 
--- route/route.js
+++ route/route.js
@@ -1,12 +1,42 @@
 
 const { render } = require("ejs");
 const express = require("express");
+const bcrypt = require('bcryptjs');
 const mongodb = require("mongodb");
-
 const db = require("../data/database");
+const { default: swal } = require("sweetalert2");
 const ObjectId = mongodb.ObjectId;
 
 const routes = express.Router();
+routes.get('/signup', function(req,res){
+  let sessionInputData = req.session.inputData;
+
+  if(!sessionInputData){
+    sessionInputData = {
+      hasError:false,
+      email: '',
+      confirmEmail: '',
+      password: '',
+    };
+  }req.session.inputData = null;
+
+  res.render('signup');
+});
+routes.get('/login', function(req,res){
+
+  let sessionInputData = req.session.inputData;
+
+  if(!sessionInputData){
+    sessionInputData = {
+      hasError:false,
+      email: '',
+      password: '',
+    };
+  }req.session.inputData = null;
+
+  res.render("login");
+});
+
 routes.post('/createNewBook', async function(req,res){
   const bookInfo = {
     title: req.body.title,
@@ -23,6 +53,7 @@ routes.post('/createNewBook', async function(req,res){
 });
 
 routes.get("/", async function (req, res) {
+
   const books = await db
     .getDb()
     .collection("book")
@@ -43,6 +74,9 @@ routes.post('/searchResults', async function(req,res){
 
 
 routes.get("/bookInfo/:id", async function (req, res) {
+  if(!req.session.isAuthenticated){
+    return res.status(401).render('401');
+  }
   const bookId = req.params.id;
   const book = await db
     .getDb()
@@ -80,6 +114,10 @@ routes.get('/searchResults', function(req,res){
 
 
 routes.get('/createNewBook', function(req,res){
+
+  if(!req.session.isAuthenticated){
+    return res.status(401).render('401');
+  }
   res.render('createNewBook');
 });
 
@@ -87,6 +125,80 @@ routes.post('/deleteBook/:id', async function(req,res){
   const bookId = req.params.id;
   const result = await db.getDb().collection('book').deleteOne({_id: new ObjectId(bookId)});
   res.json(result);
+});
+
+routes.post('/signup',async function(req,res){
+  const userData = req.body;
+  const email = userData.email;
+  const confirmEmail = userData['confirm-email'];
+  const password = userData.password;
+
+  if(!email|| !confirmEmail|| !password|| password.trim()<6|| email !== confirmEmail){
+    req.session.inputData = {
+      hasError: true,
+      message: 'invalid inputs- please check your credentials',
+      email: email,
+      confirmEmail:confirmEmail,
+      password: password
+    };
+
+    req.session.save(function(){
+      res.redirect('/signup');
+    })
+    return
+  }
+
+  const existingUser = await db.getDb().collection('users').findOne({email: email});
+
+  if(existingUser){
+   console.log('already user');
+  return res.redirect('/signup');
+  }
+
+  const hashPassword = await bcrypt.hash(password,12);
+  const user = {
+    email: email,
+    password: hashPassword
+  }
+  await db.getDb().collection('users').insertOne(user)
+  res.redirect('/login');
+});
+routes.post('/login', async function(req,res){
+  const userData = req.body;
+  const email = userData.email;
+  const password = userData.password;
+  const user = {
+    email: email,
+    password:password
+  }
+  
+  const existingUser = await db.getDb().collection('users').findOne({email: email});
+
+  if(!existingUser){
+    console.log("Not a user! create a account first");
+    return res.redirect('/login');
+  }
+
+
+  const equalPassword = await bcrypt.compare(password,existingUser.password);
+
+  if(!equalPassword){
+    console.log('wrong Password');
+    return res.redirect('/login');
+  }
+  req.session.user = {id: existingUser._id, email: existingUser.email,};
+  req.session.isAuthenticated = true;
+  req.session.save(function(){
+    res.redirect('/');
+  });
+  
+});
+
+routes.post('/logout', function(req,res){
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+
+  res.redirect('/');
 });
 
 
